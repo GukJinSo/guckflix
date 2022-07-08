5a1a7e530f9bcf0c5145788e7479ee9da3e31a6b
=gukjin <internet2dot0@gmail.com>
2022-07-08 17:58:45 +0900

    catalog

--- frontend/src/page/catalog/Catalog.js
+++ frontend/src/page/catalog/Catalog.js
@@ -13,55 +13,100 @@ const Catalog = () => {
   const { category } = useParams();
   const [searchKeyword, setSearchKeyword] = useState('');
   const [videos, setVideos] = useState([]);
+  const [params, setParams] = useState({
+    page: 1,
+  });
 
   let arr = [];
-  useEffect(() => {
-    const getVideo = async () => {
-      const params = {};
-      const response = await tmdbApi.getList(category, sortingType.popular, {
-        params,
-      });
-      response.data.results.forEach((e) => {
-        let vo = {
-          category: category,
-          name: e.title ? e.title : e.name,
-          url: e.poster_path
-            ? apiConfig.w500Image(e.poster_path)
-            : apiConfig.w500Image(e.profile_path),
-          id: e.id,
-        };
-        vo.url = vo.url === null ? noImage : vo.url;
-        arr.push(vo);
-        setVideos(arr);
-      });
-    };
-    getVideo();
-  }, []);
+
+  const getVideo = async (params) => {
+    const response = await tmdbApi.getList(category, sortingType.popular, {
+      params,
+    });
+    response.data.results.forEach((e) => {
+      let vo = {
+        category: category,
+        name: e.title ? e.title : e.name,
+        url: e.poster_path
+          ? apiConfig.w500Image(e.poster_path)
+          : apiConfig.w500Image(e.profile_path),
+        id: e.id,
+      };
+      vo.url = vo.url === null ? noImage : vo.url;
+      arr.push(vo);
+    });
+    setVideos([...videos, ...arr]);
+    arr = [];
+  };
 
   useEffect(() => {
-    console.log(setVideos);
-  }, [setVideos]);
+    console.log(videos);
+  }, [videos]);
+
+  useEffect(() => {
+    setParams({ ...params, page: 1 });
+    setVideos([]);
+    getVideo(params);
+  }, [category]);
 
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
+      console.log('enter pressed');
       setSearchKeyword(e.target.value);
     }
   };
+  const changeHandle = (e) => {
+    setSearchKeyword(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log(searchKeyword);
+  }, [searchKeyword]);
 
   const action = VideoSliderActionType.catalog;
-  const text = { catalog: '카탈로그', search: '검색' };
+  const text = {
+    catalog: '카탈로그',
+    search: '제목을 입력하세요',
+    loadMore: '더 찾기',
+  };
   return (
     <div className="catalog">
       <div className="catalog__search">
-        <div className="catalog__search__text">{text.search}</div>
-        <input value={searchKeyword} onKeyDown={handleKeyPress} type="text" />
+        <input
+          value={searchKeyword}
+          onKeyDown={handleKeyPress}
+          onChange={changeHandle}
+          type="text"
+          placeholder={`${text.search}`}
+        />
       </div>
       <div className="catalog__videos__wrap">
-        <div className="catalog__videos">
-          {videos.map((e, i) => {
-            return <VideoCard data={e} key={i} action={action} />;
-          })}
+        <div>
+          <div className="catalog__videos">
+            {videos.map((e, i) => {
+              return <VideoCard data={e} key={i} action={action} />;
+            })}
+          </div>
         </div>
+      </div>
+      <div className="catalog__loadMore">
+        <button
+          className="catalog__loadMore__button"
+          onClick={() => {
+            setParams({ ...params, page: params.page + 1 });
+            getVideo(params);
+          }}
+        >
+          {text.loadMore}
+        </button>
+        <button
+          className="catalog__loadMore__button"
+          onClick={() => {
+            setVideos([]);
+          }}
+        >
+          올삭제
+        </button>
       </div>
     </div>
   );
